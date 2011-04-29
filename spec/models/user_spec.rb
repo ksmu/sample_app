21e07c38740cf66f4146d2f19285b879b0368fbe
velsa <samuel.velkey@gmail.com>
2011-04-29 13:56:17 +0200

    Finished first cut of the User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -34,6 +34,15 @@ describe User do
 		end
 	end
 	
+	it "should reject invalid email addresses" do
+		addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+		addresses.each do |address|
+			invalid_email_user = User.new(@attr.merge(:email => address))
+			invalid_email_user.should_not be_valid
+		end
+	end
+	
+	
 	it "should reject duplicate email address" do
 		#Put a user with given email address into the database.
 		User.create!(@attr)
