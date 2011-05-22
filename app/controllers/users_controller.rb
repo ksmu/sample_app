9abba68e756a82f16b3dc2ffcdf0c0a6c69244dd
velsa <samuel.velkey@gmail.com>
2011-05-22 23:12:43 +0200

    Done with user edit/update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+	before_filter :authenticate, :only 	=> [:edit, :edit, :update]
+	before_filter :correct_user, :only 	=> [:edit, :update]
+	before_filter :admin_user,	 :only	=> :destroy
 	
 	def show
 		@user = User.find(params[:id])
@@ -9,6 +12,16 @@ class UsersController < ApplicationController
 		@user = User.new
 		@title = "Sign up"
 	end
+	
+	def index
+		@title = "All users"
+		@users = User.paginate(:page => params[:page])
+	end
+	def show
+		@user = User.find(params[:id])
+		@title = @user.name
+	end
+	
 	
 	def create
 		@user = User.new(params[:user])
@@ -21,6 +34,41 @@ class UsersController < ApplicationController
 			@title = "Sign up"
 			render 'new'
 		end
+	end
+	
+	def edit
+		@user = User.find(params[:id])
+		@title = "Edit user"
+	end
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(params[:user])
+		      flash[:success] = "Profile updated."
+		      redirect_to @user
+		else
+		      @title = "Edit user"
+		      render 'edit'
+		end
+	end
+	
+	def destroy
+		User.find(params[:id]).destroy
+		flash[:success] = "User destroyed"
+		redirect_to users_path
+	end
+	
+private
+	def authenticate
+		deny_access unless signed_in?
+	end
+	
+	def correct_user
+		@user = User.find(params[:id])
+		redirect_to(root_path) unless current_user?(@user)
+	end
+	
+	def admin_user
+		redirect_to(root_path) unless current_user.admin?
 	end
 	
 end
