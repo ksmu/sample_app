6a21c9ebf072d08af2fd56e315251c2f43c97dd3
velsa <samuel.velkey@gmail.com>
2011-06-24 20:36:54 +0200

    ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-	before_filter :authenticate, :only 	=> [:edit, :edit, :update]
+	before_filter :authenticate, :only 	=> [:index, :edit, :update,:destroy]
 	before_filter :correct_user, :only 	=> [:edit, :update]
 	before_filter :admin_user,	 :only	=> :destroy
 	
@@ -57,7 +57,7 @@ class UsersController < ApplicationController
 		redirect_to users_path
 	end
 	
-private
+
 	def authenticate
 		deny_access unless signed_in?
 	end
